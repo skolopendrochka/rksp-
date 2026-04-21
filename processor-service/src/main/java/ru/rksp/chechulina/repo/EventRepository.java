04834c2dd2c8459c6c502b554cf6f237098cbe46
skolopendrochka <katya.chech@yandex.ru>
2026-04-21 17:09:02 +0300

    Delete processor-service/src/main directory

--- processor-service/src/main/java/ru/rksp/chechulina/repo/EventRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.rksp.chechulina.repo;
-
-import ru.rksp.chechulina.entity.EventEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface EventRepository extends JpaRepository<EventEntity, UUID> {
-}
